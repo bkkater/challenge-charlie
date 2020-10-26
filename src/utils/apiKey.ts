feafb0e5354d80d679c93dd745c7bd80a7479a6e
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-26 13:02:10 -0300

    [add] utils file

---
+++ src/utils/apiKey.ts
@@ -0,0 +1,5 @@
+export const key = {
+    openWeatherKey: 'fdfd5db5b8344436d3d2019afdb9f1b5',
+    openCageKey: 'c63386b4f77e46de817bdf94f552cddf'
+    //yeah, i know that this key is exposed, you can use little hacker
+}
